4e8e2a632f0142a739f53f7afe98e10329996b83
xnitronq <xnitronq@gmail.com>
2018-09-04 23:52:51 +0300

    Attr

--- ManagePass/Controller/ControllerPass.cs
+++ ManagePass/Controller/ControllerPass.cs
@@ -14,32 +14,15 @@ namespace ManagerPass
 
     public class Controller
     {
-        
         Model modelPass = new Model();
         NameValueCollection newPass = new NameValueCollection();
 
-       
         public void AddPass(View viewPass)
         {
-            modelPass.BinFile();
             newPass.Add(viewPass.Site, viewPass.Password);
-            IFormatter formatter = new BinaryFormatter();
-            using (FileStream s = File.OpenWrite(modelPass.filename))
-                formatter.Serialize(s, newPass);
+            modelPass.AddPass(newPass);
         }
-   
-        public void ShowPass()
-        {
-            newPass.Clear();
-            modelPass.BinFile();
-            IFormatter formatter = new BinaryFormatter();
-            using (FileStream s = File.OpenRead(modelPass.filename))
-            {
-                newPass.Add((NameValueCollection)formatter.Deserialize(s));
-                for (int i = 0; i < newPass.Count; i++)
-                    Console.WriteLine("{0}  -  {1}", newPass.GetKey(i), newPass.Get(i));
-            }
-        }
+
     }
    
 }
--- ManagePass/Model/ModelPass.cs
+++ ManagePass/Model/ModelPass.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,15 +14,44 @@ namespace ManagerPass
     public class Model
     {
         public readonly string filename = "serialized.bin";
+        NameValueCollection newPass = new NameValueCollection();
         public Model ()
         {
-            if (!File.Exists(filename))
-                using (FileStream fs = File.Create(filename)) ;
+
         }
         public void BinFile()
         {
-            
+            if (!File.Exists(filename))
+                using (FileStream fs = File.Create(filename))
+                    File.SetAttributes(filename, FileAttributes.Hidden); 
+        }
+        public void AddPass(NameValueCollection newPass)
+        {
+            this.BinFile();
+            IFormatter formatter = new BinaryFormatter();
+            using (FileStream s = File.OpenWrite(filename))
+                formatter.Serialize(s, newPass);
+        }
+        public void ShowPass()
+        {
+            this.BinFile();
+            newPass.Clear();
+            IFormatter formatter = new BinaryFormatter();
+            using (FileStream s = File.OpenRead(filename))
+            {
+                try
+                {
+                    newPass.Add((NameValueCollection)formatter.Deserialize(s));
+                    for (int i = 0; i < newPass.Count; i++)
+                        Console.WriteLine("{0}  -  {1}", newPass.GetKey(i), newPass.Get(i));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("You do not have any passwords");
+                }
                 
+            }
         }
     }
 }
+    
